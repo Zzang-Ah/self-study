76f23e06156b184cb253987431765c51a699a4e9
Zzang-Ah <135772927+Zzang-Ah@users.noreply.github.com>
2023-07-13 11:13:51 +0900

    파이썬 유튭강의 예제 1부터10

---
+++ 01_10.py
@@ -0,0 +1,14 @@
+# 1번: print("Hello World")
+# 2번: print("Mary's cosmetics")
+# 2번 문제에 대한 추가 : print('Mary's cosmetics') 하게되면 컴퓨터가 str어디까지인지 인식을 못함
+# 3번: print('신씨가 소리를 질렀다. "도둑이야."')
+# 4번: print("a\tb") 파이썬에서 \를 표현하려면 \\ (역슬레시 한번 더 붙여야함)
+# 5번: print("안녕하세요. \n만나서\t\t반갑습니다.")
+# 5번 추가설명: print 탭과 줄바꿈 => \t tap(들여쓰기)이다. \n (줄바꿈)
+# 6번: print("오늘은", "일요일")
+# 7번: print("naver", "kakao", "sk", "samsung", sep=';')
+# 8번: print("naver", "kakao", "sk", "samsung", sep='/')
+# 9번: 줄바꿈 print("first");print("second")
+# 9번 추가: 이어서 써짐 print("first", end="");print("second")
+# 10번: print(5/3)
+
